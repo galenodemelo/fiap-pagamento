d4592c86da99d02cb8aecb8863cdd904eeafe129
Galeno de Melo <galenodemelo@gmail.com>
2024-01-19 13:41:32 -0300

    feat: Tabela para cadastrar as chaves de idempotência

---
+++ src/main/java/com/fiap/restaurant/external/db/idempotency/IdempotencyKeyRepository.java
@@ -0,0 +1,12 @@
+package com.fiap.restaurant.external.db.idempotency;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+@EnableScan
+public interface IdempotencyKeyRepository extends CrudRepository<IdempotencyKey, String> {
+
+    Optional<IdempotencyKey> findById(String id);
+}
