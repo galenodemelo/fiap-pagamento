0853369bb5751e5c5c18feb5083ced945fd77784
Galeno de Melo <galenodemelo@gmail.com>
2024-01-19 13:37:47 -0300

    feat: Configurando bean do DynamoDB

---
+++ src/main/java/com/fiap/restaurant/external/db/config/DynamoDBConfig.java
@@ -0,0 +1,60 @@
+package com.fiap.restaurant.external.db.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Configuração baseada <a href="https://siecola.com.br/blogs/aws_ddb_java.html">neste post</a>
+ * e <a href="https://www.baeldung.com/spring-data-dynamodb">neste outro</a>
+ */
+
+@Configuration
+@EnableDynamoDBRepositories(basePackages = "com.fiap.restaurant.external.db")
+public class DynamoDBConfig {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Value("${aws.dynamodb.endpoint}")
+    private String endpoint;
+
+    @Value("${aws.dynamodb.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.dynamodb.secretKey}")
+    private String secretKey;
+
+    @Bean
+    @Primary
+    public DynamoDBMapperConfig dynamoDBMapperConfig() {
+        return DynamoDBMapperConfig.DEFAULT;
+    }
+
+    @Bean
+    @Primary
+    public AmazonDynamoDB amazonDynamoDB() {
+        AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey));
+
+        AmazonDynamoDBClientBuilder clientBuilder = AmazonDynamoDBClientBuilder.standard();
+        clientBuilder.withCredentials(credentialsProvider);
+        clientBuilder.setEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, awsRegion));
+
+        return clientBuilder.build();
+    }
+
+    @Bean
+    @Primary
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB, DynamoDBMapperConfig config) {
+        return new DynamoDBMapper(amazonDynamoDB, config);
+    }
+}
