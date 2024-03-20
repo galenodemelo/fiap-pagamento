545d6e80242c22f51bdae902bf2ffc788cc35380
Galeno de Melo <galenodemelo@gmail.com>
2024-03-19 21:38:13 -0300

    feat: Envia resposta do pagamento para uma fila do SQS

--- src/main/java/com/fiap/restaurant/controller/PaymentController.java
+++ src/main/java/com/fiap/restaurant/controller/PaymentController.java
@@ -1,15 +1,27 @@
 package com.fiap.restaurant.controller;
 
+import com.fiap.restaurant.external.messagebroker.MessageBroker;
+import com.fiap.restaurant.external.messagebroker.SqsMessageBroker;
 import com.fiap.restaurant.gateway.IPaymentGateway;
 import com.fiap.restaurant.gateway.PaymentGateway;
 import com.fiap.restaurant.types.db.PaymentDatabaseConnection;
 import com.fiap.restaurant.types.dto.SavePaymentDTO;
 import com.fiap.restaurant.usecase.PaymentUseCase;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PaymentController {
 
+    private final PaymentDatabaseConnection paymentDatabaseConnection;
+
+    public PaymentController(PaymentDatabaseConnection paymentDatabaseConnection) {
+        this.paymentDatabaseConnection = paymentDatabaseConnection;
+    }
+
     public static void save(SavePaymentDTO savePaymentDTO, PaymentDatabaseConnection paymentDatabaseConnection) {
-        IPaymentGateway paymentGateway = new PaymentGateway(paymentDatabaseConnection);
+        MessageBroker messageBroker = new SqsMessageBroker("response-q");
+
+        IPaymentGateway paymentGateway = new PaymentGateway(paymentDatabaseConnection, messageBroker);
         PaymentUseCase.save(savePaymentDTO, paymentGateway);
     }
 }
--- src/main/java/com/fiap/restaurant/gateway/PaymentGateway.java
+++ src/main/java/com/fiap/restaurant/gateway/PaymentGateway.java
@@ -1,26 +1,47 @@
 package com.fiap.restaurant.gateway;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fiap.restaurant.entity.Payment;
 import com.fiap.restaurant.external.db.payment.PaymentJpa;
+import com.fiap.restaurant.external.messagebroker.MessageBroker;
 import com.fiap.restaurant.types.db.PaymentDatabaseConnection;
+import com.fiap.restaurant.types.dto.service.ServiceResponseQueueDTO;
+import com.fiap.restaurant.types.dto.service.ServiceResponseQueueType;
 import com.fiap.restaurant.types.mapper.PaymentMapper;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("unchecked")
 public class PaymentGateway implements IPaymentGateway {
 
     private final PaymentDatabaseConnection paymentDatabaseConnection;
 
-    public PaymentGateway(PaymentDatabaseConnection paymentDatabaseConnection) {
+    private final MessageBroker messageBroker;
+
+    public PaymentGateway(PaymentDatabaseConnection paymentDatabaseConnection, MessageBroker messageBroker) {
         this.paymentDatabaseConnection = paymentDatabaseConnection;
+        this.messageBroker = messageBroker;
     }
 
     @Override
-    public Payment save(Payment payment) {
+    @Transactional
+    public Payment save(Payment payment) throws JsonProcessingException {
         PaymentJpa paymentJpa = new PaymentJpa();
         paymentJpa.setOrderId(payment.getOrderId());
         paymentJpa.setValue(payment.getValue());
 
         paymentJpa = this.paymentDatabaseConnection.save(paymentJpa);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonData = objectMapper.writeValueAsString(new ServiceResponseQueueDTO(
+            ServiceResponseQueueType.ORDER_PAYMENT_FINISHED,
+            Map.of("orderId", payment.getOrderId())
+        ));
+
+        this.messageBroker.send(jsonData);
 
         return PaymentMapper.INSTANCE.toPayment(paymentJpa);
     }
